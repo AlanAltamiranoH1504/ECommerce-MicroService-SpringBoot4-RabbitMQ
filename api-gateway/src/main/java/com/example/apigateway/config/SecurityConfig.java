689f5f3cf7a19c56b90c3eea3d5e378fa45533e7
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-06-20 12:32:48 -0600

    feat: add JWT Role Converter and Path Protection by Roles - add Key Cloak JWT to JWT Spring Security Converter Method - add MS path protection by roles

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -2,9 +2,18 @@ package com.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,10 +24,48 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/eureka/**").permitAll()
+
+                        // * RUTAS DE PRODUCTOS
+                        .pathMatchers(HttpMethod.GET, "/products/**").permitAll()
+                        .pathMatchers("/products/**").hasRole("ADMIN")
+
+                        // * RUTAS DE INVENTARIOS
+                        .pathMatchers(HttpMethod.GET, "/inventory/**").permitAll()
+                        .pathMatchers("/inventory/**").hasRole("ADMIN")
+
+                        // * RUTAS DE ORDENES
+                        .pathMatchers(HttpMethod.POST, "/orders").hasRole("USER")
+                        .pathMatchers("/orders/**").hasRole("ADMIN")
+
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtSpec -> {
-                }));
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        // * UTILIZAMOS EL CONVERTIDOR DE ROLES DE JWT
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(this.reactiveJwtAuthenticationConverterAdapter())));
         return http.build();
+    }
+
+    // * METODO TRADUCTOR DE ROLES ENTRE SPRING SECURITY Y KEYCLOAK
+    private ReactiveJwtAuthenticationConverterAdapter reactiveJwtAuthenticationConverterAdapter() {
+        // * GENERACION DE CONVERTIDOR
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            // * DEL JWT QUE DA KEYCLOAK, SACAMOS LOS real_access
+            Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+
+            // * VALIDACION DE QUE LOS REAL_ACCESS EXISTAN
+            if (realmAccess == null || realmAccess.isEmpty()) {
+                return Collections.emptyList();
+            }
+
+            // * SACAMOS LOS ROLES
+            Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+            return roles.stream()
+                    .map(rol -> new SimpleGrantedAuthority("ROLE_" + rol))
+                    .collect(Collectors.toList());
+        });
+
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
     }
 }
